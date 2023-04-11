78bb6bbb6626540c535723c587c7e4be8e4377bf
Ahmed Ruda <115115962+arudz55@users.noreply.github.com>
2023-04-11 19:54:53 -0400

    ALL THE ERROR CHECKING

--- DP-2/src/dataProducer-2.c
+++ DP-2/src/dataProducer-2.c
@@ -177,7 +177,10 @@ int init_semaphore(int *semID)
     }
 
     // initialize the semaphore value to 1
-    semctl(*semID, 0, SETVAL, 1);
+    if (semctl(*semID, 0, SETVAL, 1) == -1) {
+        perror("DC semctl");
+        return 1;
+    }
     return 0;
 }
 
